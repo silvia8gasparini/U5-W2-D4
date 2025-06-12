7b812df9297c29c07b57efdeea2212246937e18e
Silvia <silvia8gasparini@gmail.com>
2025-06-12 14:07:08 +0200

    create pj

---
+++ src/main/java/it/epicode/U5W2D4practice/U5W2D4practiceApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.U5W2D4practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D4practiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D4practiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/U5W2D4practice/U5W2D4practiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.U5W2D4practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D4practiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
